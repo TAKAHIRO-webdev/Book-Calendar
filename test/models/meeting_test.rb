2fd93e2a505625bff91b07daa7c4786d864cf8cf
Ubuntu <ubuntu@ip-172-31-33-124.ap-northeast-1.compute.internal>
2019-11-13 08:16:29 +0000

    Added minitest-reportter

--- test/models/meeting_test.rb
+++ test/models/meeting_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MeetingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @meeting = Meeting.new(
+                           name: "I am a Cat", 
+                           start_time: "2019-11-13 07:31:00",
+                           end_time: "user@example.com",
+                           start_page: 10,
+                           end_page: 20,
+                           content: "I AM A CAT. As yet I have no name. I've no idea where I was born."
+                           )
+  end
+  
+  test "should be valid" do
+    assert @meeting.valid?
+  end
+  
 end
